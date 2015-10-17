fcc9cb0ee96c1592d1367fea88d1e737bac56925
leonmf <leongrossman@gmail.com>
2015-10-17 08:54:58 -0500

    Initial Commit

---
+++ S3DFix/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  S3DFix
+//
+//  Created by LEON GROSSMAN on 10/17/15.
+//  Copyright © 2015 LEON GROSSMAN. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
