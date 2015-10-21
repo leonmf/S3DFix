45b0c0a8c85eca14d9d47a911713a0f6814998a6
leonmf <leongrossman@gmail.com>
2015-10-21 07:19:47 -0500

    third initial commit

---
+++ S3DFix/RootViewController.swift
@@ -0,0 +1,57 @@
+//
+//  RootViewController.swift
+//  S3DFix
+//
+//  Created by LEON GROSSMAN on 10/17/15.
+//  Copyright © 2015 LEON GROSSMAN. All rights reserved.
+//
+
+import Cocoa
+
+class RootViewController: NSViewController {
+
+    @IBOutlet weak var labelStatus: NSTextField!
+    
+    let S3DParse = ProcessFileModel(resolutionThreshold: 0.01, extrusionThreshold: 0.001)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+ 
+    
+    @IBAction func processFile(sender: AnyObject) {
+        
+        let fileName = openFile()
+        
+        if fileName != nil {
+            labelStatus.stringValue = "Processing \(fileName!)"
+            
+            
+            let result = S3DParse.Process(fileName!)
+            //let result = fix.Process(fileName!)
+            labelStatus.stringValue = result
+        } else {
+            labelStatus.stringValue = "Please select a valid file to process"
+        }
+    }
+    
+    
+    override func awakeFromNib() {
+        //labelStatus.stringValue = "Test"
+    }
+    
+    func openFile() -> String? {
+        
+        let myFileDialog: NSOpenPanel = NSOpenPanel()
+        myFileDialog.runModal()
+        
+        // Get the path to the file chosen in the NSOpenPanel
+        let path = myFileDialog.URL?.path
+        return path
+        
+        // Make sure that a path was chosen
+        
+        
+    }
+}
